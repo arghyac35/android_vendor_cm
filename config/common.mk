a260a566f9f1a0595f227e0a98509f18546f7df8
Martin Pollard <pmart303@ford.com>
2017-10-18 09:10:32 -0400

    [DUI] Add DUI prebuilts

--- config/common.mk
+++ config/common.mk
@@ -143,6 +143,13 @@ PRODUCT_PACKAGES += \
     WallpaperPicker \
     WeatherProvider
 
+# DU Utils Library
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
+
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
 # Exchange support
 PRODUCT_PACKAGES += \
     Exchange2
